91d6620bc4c1c39d6a579cbc02cc1d668bc34605
PedroUlian <83662297+PedroUlian@users.noreply.github.com>
2025-10-04 16:08:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,13 @@ def get_messages(limit=50):
 
 # ============================
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
 @app.route("/")
 def index():
     return "Servidor RPG Chat rodando!"
