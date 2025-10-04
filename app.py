593fee92acd48dbab2340d2170ac4a88623a9d91
PedroUlian <83662297+PedroUlian@users.noreply.github.com>
2025-10-04 17:02:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import sqlite3
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-CORS(app)
 app.config["SECRET_KEY"] = "segredo_rpg"
 
 socketio = SocketIO(app, cors_allowed_origins="*")
