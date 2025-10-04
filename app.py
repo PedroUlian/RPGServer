d01e9eda6ca4248f87707e341baf43cddfd544e6
PedroUlian <83662297+PedroUlian@users.noreply.github.com>
2025-10-04 12:53:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_socketio import SocketIO, send
 
@@ -7,12 +8,12 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on("message")
 def handle_message(data):
-    # data é um dict: {user: "nome", text: "mensagem"}
     user = data.get("user", "Anon")
     text = data.get("text", "")
     print(f"Mensagem de {user}: {text}")
     send({"user": user, "text": text}, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, use_reloader=True, log_output=True, allow_unsafe_werkzeug=True)
-
+    # Render define a porta em uma variável de ambiente
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, use_reloader=True, log_output=True, allow_unsafe_werkzeug=True)
